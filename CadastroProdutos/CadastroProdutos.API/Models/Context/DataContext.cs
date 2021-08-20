8735ac3cdf4863fa28812152370507b40d6c9c65
Danilo Silva <danilo.silva@msn.com>
2021-08-20 00:27:32 -0300

    Definindo o DbContextOptions

--- CadastroProdutos/CadastroProdutos.API/Models/Context/DataContext.cs
+++ CadastroProdutos/CadastroProdutos.API/Models/Context/DataContext.cs
@@ -4,6 +4,8 @@ namespace CadastroProdutos.API.Models
 {
     public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) { }
+
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Produto> Produtos { get; set; }
     }
