22fa117b3c8e6bf4c200b23374d9909b0cb64473
Danilo Silva <danilo.silva@msn.com>
2021-08-20 00:38:38 -0300

    Registrando o AddDbContext como serviço

--- CadastroProdutos/CadastroProdutos.API/Startup.cs
+++ CadastroProdutos/CadastroProdutos.API/Startup.cs
@@ -1,7 +1,9 @@
+using CadastroProdutos.API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,7 +27,11 @@ namespace CadastroProdutos.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionStringMySQL = Configuration.GetConnectionString("MySQLConnection");
 
+            services.AddDbContext<DataContext>(options =>
+                options.UseMySql(connectionStringMySQL, ServerVersion.AutoDetect(connectionStringMySQL))
+            );
             services.AddControllers();
         }
 
