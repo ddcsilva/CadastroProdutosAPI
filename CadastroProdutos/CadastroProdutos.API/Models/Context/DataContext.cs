052d5bc2374ec98c7b6c424c1ce9a4b01de7455d
Danilo Silva <danilo.silva@msn.com>
2021-08-19 23:35:04 -0300

    Criação da classe de Contexto

---
+++ CadastroProdutos/CadastroProdutos.API/Models/Context/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CadastroProdutos.API.Models
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
