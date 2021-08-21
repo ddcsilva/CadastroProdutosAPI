287a906481551fe61653386773ac5b1f8cf89346
Danilo Silva <danilo.silva@msn.com>
2021-08-21 02:07:17 -0300

    Configurando Models

--- CadastroProdutos/CadastroProdutos.API/Models/Categoria.cs
+++ CadastroProdutos/CadastroProdutos.API/Models/Categoria.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CadastroProdutos.API.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
         public Categoria()
@@ -10,7 +13,11 @@ namespace CadastroProdutos.API.Models
             Produtos = new Collection<Produto>();
         }
 
+        [Key]
         public int CategoriaId { get; set; }
+
+        [Required]
+        [MaxLength(80)]
         public string Nome { get; set; }
 
         public ICollection<Produto> Produtos { get; set; }
--- CadastroProdutos/CadastroProdutos.API/Models/Produto.cs
+++ CadastroProdutos/CadastroProdutos.API/Models/Produto.cs
@@ -1,13 +1,27 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CadastroProdutos.API.Models
 {
+    [Table("Produtos")]
     public class Produto
     {
+        [Key]
         public int ProdutoId { get; set; }
+
+        [Required]
+        [MaxLength(80)]
         public string Nome { get; set; }
+
         public string Descricao { get; set; }
+
+        [Required]
+        [MaxLength(80)]
         public decimal Preco { get; set; }
+
+        [Required]
+        [MaxLength(80)]
         public string ImagemUrl { get; set; }
         public int Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
