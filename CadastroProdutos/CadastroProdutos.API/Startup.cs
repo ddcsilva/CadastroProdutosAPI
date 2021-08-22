a9d4a21ced5716ea85b315f76be4a8a68223e558
Danilo Silva <danilo.silva@msn.com>
2021-08-22 16:53:23 -0300

    Configuração para ignorar Evento Cíclico

--- CadastroProdutos/CadastroProdutos.API/Startup.cs
+++ CadastroProdutos/CadastroProdutos.API/Startup.cs
@@ -1,17 +1,10 @@
 using CadastroProdutos.API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CadastroProdutos.API
 {
@@ -32,7 +25,11 @@ namespace CadastroProdutos.API
             services.AddDbContext<DataContext>(options =>
                 options.UseMySql(connectionStringMySQL, ServerVersion.AutoDetect(connectionStringMySQL))
             );
-            services.AddControllers();
+            services.AddControllers()
+                    .AddNewtonsoftJson(options => 
+                    {
+                        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                    });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
